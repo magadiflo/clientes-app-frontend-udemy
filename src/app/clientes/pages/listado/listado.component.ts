8fc71932caa72f3205797490f7b623a27df59b7a
Martín <magadiflo@gmail.com>
2022-04-29 18:54:06 -0500

    Creando archivo clientes.data.ts con la lista de objetos de Clientes

--- src/app/clientes/pages/listado/listado.component.ts
+++ src/app/clientes/pages/listado/listado.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Cliente } from '../../interfaces/cliente.interface';
+import { CLIENTES } from '../../data/clientes.data';
 
 @Component({
   selector: 'app-listado',
@@ -9,19 +10,7 @@ import { Cliente } from '../../interfaces/cliente.interface';
 })
 export class ListadoComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    { id: 1, nombre: 'Martín', apellido: 'Díaz', createAt: '2022-02-24', email: 'magadiflo@gmail.com' },
-    { id: 2, nombre: 'Gaspar', apellido: 'Flores', createAt: '2022-02-22', email: 'gaspar@gmail.com' },
-    { id: 3, nombre: 'Alicia', apellido: 'Zúñiga', createAt: '2022-02-20', email: 'alicia@gmail.com' },
-    { id: 4, nombre: 'Tinkler', apellido: 'Díaz', createAt: '2022-02-02', email: 'tinkler@gmail.com' },
-    { id: 5, nombre: 'Raúl', apellido: 'Flores', createAt: '2022-02-23', email: 'raul@gmail.com' },
-    { id: 6, nombre: 'Gabriel', apellido: 'Díaz', createAt: '2022-02-27', email: 'gabriel@gmail.com' },
-    { id: 7, nombre: 'Gabrielito', apellido: 'Díaz', createAt: '2022-02-26', email: 'gabrielito@gmail.com' },
-    { id: 8, nombre: 'Gahella', apellido: 'Díaz', createAt: '2022-02-25', email: 'gahella@gmail.com' },
-    { id: 9, nombre: 'Marilyn', apellido: 'Ardiles', createAt: '2022-02-14', email: 'marilyn@gmail.com' },
-    { id: 10, nombre: 'Violeta', apellido: 'Flores', createAt: '2022-02-16', email: 'violeta@gmail.com' },
-    { id: 11, nombre: 'Adán', apellido: 'Díaz', createAt: '2022-02-10', email: 'adan@gmail.com' },
-  ];
+  clientes: Cliente[] = CLIENTES;
 
   constructor() { }
 
