28ec546e692e64251b3064a249c71ab5b62bc94d
Martín <magadiflo@gmail.com>
2022-05-02 12:12:18 -0500

    Implementando Servicio Angular con HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -23,6 +24,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/clientes/services/cliente.service.ts
+++ src/app/clientes/services/cliente.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
-import { CLIENTES } from '../data/clientes.data';
 import { Cliente } from '../interfaces/cliente.interface';
+import { environment } from '../../../environments/environment.prod';
+
+const BASE_URL = environment.baseUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    return this.http.get<Cliente[]>(`${BASE_URL}/api/clientes`);
   }
 }
