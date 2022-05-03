338525ed54a83169229523a284a25f782b7dcdc1
Martín <magadiflo@gmail.com>
2022-05-03 10:40:32 -0500

    Manejo de error en el Frontend Angular en obtener por id

--- src/app/clientes/services/cliente.service.ts
+++ src/app/clientes/services/cliente.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
+import Swal from 'sweetalert2';
+
+import { environment } from '../../../environments/environment';
 import { Cliente } from '../interfaces/cliente.interface';
-import { environment } from '../../../environments/environment.prod';
 
 const BASE_URL = environment.baseUrl;
 
@@ -14,14 +17,24 @@ export class ClienteService {
 
   private httpHeaders: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router) { }
 
   getClientes(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>(`${BASE_URL}/api/clientes`);
   }
 
   getCliente(id: number): Observable<Cliente> {
-    return this.http.get<Cliente>(`${BASE_URL}/api/clientes/${id}`);
+    return this.http.get<Cliente>(`${BASE_URL}/api/clientes/${id}`)
+      .pipe(
+        catchError(e => {
+          console.log(e);
+          this.router.navigate(['/clientes']);
+          Swal.fire('Error al obtener el cliente', e.error.mensaje, 'error');
+          return throwError(() => e);
+        })
+      );
   }
 
   create(cliente: Cliente): Observable<Cliente> {
