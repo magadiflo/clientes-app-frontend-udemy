4b3de159f912a592e95f40233e011e020871f58d
Martín <magadiflo@gmail.com>
2022-05-05 10:29:52 -0500

    Validando imagen antes de subir

--- src/app/clientes/pages/detalle/detalle.component.ts
+++ src/app/clientes/pages/detalle/detalle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import Swal from 'sweetalert2';
@@ -16,7 +16,10 @@ export class DetalleComponent implements OnInit {
 
   cliente!: Cliente;
   titulo: string = 'Información del cliente';
-  private fotoSeleccionada!: File;
+  fotoSeleccionada!: File | null;
+
+  @ViewChild('inputFoto')
+  inputFoto!: ElementRef;
 
   constructor(
     private clienteService: ClienteService,
@@ -36,20 +39,36 @@ export class DetalleComponent implements OnInit {
   }
 
   seleccionarFoto(event: any) {
-    console.log(event);
     this.fotoSeleccionada = event.target!.files[0];
     console.log(this.fotoSeleccionada);
+    if (this.fotoSeleccionada!.type.indexOf('image') < 0) {
+      Swal.fire('Error Seleccionar imagen', 'El archivo debe ser del tipo imagen', 'error');
+      this.fotoSeleccionada = null;
+      this._reset();
+    }
   }
 
   subirFoto() {
-    this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id!)
-      .subscribe(cliente => {
-        this.cliente = cliente;
-        Swal.fire(
-          'La foto se ha subido completamente!',
-          `La foto se ha subido con éxito ${this.cliente.foto}`,
-          'success');
-      });
+    if (!this.fotoSeleccionada) {
+      Swal.fire('Error upload', 'Debe seleccionar una foto', 'error');
+    } else {
+      this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id!)
+        .subscribe(cliente => {
+          this.cliente = cliente;
+          this.fotoSeleccionada = null;
+          this._reset();
+          Swal.fire(
+            'La foto se ha subido completamente!',
+            `La foto se ha subido con éxito ${this.cliente.foto}`,
+            'success');
+        });
+    }
+  }
+
+  private _reset(): void {
+    console.log(this.inputFoto.nativeElement.files);
+    this.inputFoto.nativeElement.value = '';
+    console.log(this.inputFoto.nativeElement.files);
   }
 
 }
