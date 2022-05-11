d132cb8050a4f788b2ba4f9adfa3df886a7ebdb2
Martín <magadiflo@gmail.com>
2022-05-11 00:21:01 -0500

    Manejo de error no autorizado y redirección al login

--- src/app/clientes/services/cliente.service.ts
+++ src/app/clientes/services/cliente.service.ts
@@ -12,6 +12,8 @@ import { Region } from '../interfaces/region.interface';
 
 const BASE_URL = environment.baseUrl;
 const BAD_REQUEST: number = 400;
+const UNAUTHORIZED: number = 401;
+const FORBIDDEN: number = 403;
 
 @Injectable({
   providedIn: 'root'
@@ -24,8 +26,22 @@ export class ClienteService {
     private http: HttpClient,
     private router: Router) { }
 
+  private isNoAutorizado(e: any): boolean {
+    if (e.status == UNAUTHORIZED || e.status == FORBIDDEN) {
+      this.router.navigate(['/login']);
+      return true;
+    }
+    return false;
+  }
+
   getRegiones(): Observable<Region[]> {
-    return this.http.get<Region[]>(`${BASE_URL}/api/clientes/regiones`);
+    return this.http.get<Region[]>(`${BASE_URL}/api/clientes/regiones`)
+      .pipe(
+        catchError((e: any) => {
+          this.isNoAutorizado(e);
+          return throwError(() => e);
+        })
+      );
   }
 
   getClientes(): Observable<Cliente[]> {
@@ -57,6 +73,9 @@ export class ClienteService {
     return this.http.get<Cliente>(`${BASE_URL}/api/clientes/${id}`)
       .pipe(
         catchError(e => {
+          if (this.isNoAutorizado(e)) {
+            return throwError(() => e);
+          }
           console.log(e);
           this.router.navigate(['/clientes']);
           Swal.fire('Error al obtener el cliente', e.error.mensaje, 'error');
@@ -70,6 +89,9 @@ export class ClienteService {
       .pipe(
         map(({ cliente }) => cliente),
         catchError(e => {
+          if (this.isNoAutorizado(e)) {
+            return throwError(() => e);
+          }
           if (e.status == BAD_REQUEST) {
             return throwError(() => e);
           }
@@ -85,6 +107,9 @@ export class ClienteService {
       .pipe(
         map(({ cliente }) => cliente),
         catchError(e => {
+          if (this.isNoAutorizado(e)) {
+            return throwError(() => e);
+          }
           if (e.status == BAD_REQUEST) {
             return throwError(() => e);
           }
@@ -100,6 +125,9 @@ export class ClienteService {
       .pipe(
         map(({ mensaje }) => mensaje),
         catchError(e => {
+          if (this.isNoAutorizado(e)) {
+            return throwError(() => e);
+          }
           console.log(e);
           Swal.fire(e.error.mensaje, e.error.error, 'error');
           return throwError(() => e);
@@ -107,7 +135,7 @@ export class ClienteService {
       );
   }
 
-  subirFoto(archivo: File, id: number): Observable<HttpEvent<{}>> {
+  subirFoto(archivo: File, id: number): Observable<HttpEvent<any>> {
     let formData = new FormData(); //Clase nativa de JavaScript
     formData.append("archivo", archivo);
     formData.append("id", id.toString());
@@ -116,6 +144,15 @@ export class ClienteService {
       reportProgress: true
     });
 
-    return this.http.request(req);
+    return this.http.request(req)
+      .pipe(
+        catchError(e => {
+          //** Al redireccionar queda el fondo del modal, con esto lo eliminamos
+          document.getElementsByClassName('modal-backdrop')[0].remove();
+          console.log('Error al subir la foto', e);
+          this.isNoAutorizado(e);
+          return throwError(() => e);
+        })
+      );
   }
 }
